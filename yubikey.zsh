7aeeea6ec291a4d1ab99c0b139435b7483080a8a
bart@bartlegian <bart@bamweb.nl>
2016-07-17 23:14:52 +0200

    yubikey for zsh

---
+++ yubikey.zsh
@@ -0,0 +1,24 @@
+# make it less tempting to use gpg version 1
+alias gpg=gpg2
+# use gpg's tab completions for gpg2; careful: not all options exist in both
+compdef gpg2=gpg
+
+# Yubikey , GPG, SSH
+if [ -x /usr/bin/gpg-agent ]; then
+  echo "gpg-agent exists"
+  if [[ ! $(ps x | grep gpg-agent | grep ssh | grep -v grep) ]]; then
+    echo "no gpg-agent exists"
+    while [[ $(ps x | grep gpg-agent | grep -v grep) ]]; do
+      echo "trying to kill the agent"
+      killall gpg-agent > /dev/null 2>&1
+    done
+    #if [ ! -f $HOME/.gpg-agent-info ]; then
+    #  echo "filling .gpg-agent-info"
+      eval $(gpg-agent --daemon --enable-ssh-support > $HOME/.gpg-agent-info);
+    #fi
+  fi
+  # execute its contents, including the export
+  test -f $HOME/.gpg-agent-info && source $HOME/.gpg-agent-info
+fi
+# End yubikey settings
+
