9d934d6efff6ec38d659a4e164de1aa83f969581
bart@bartlegian <bart@bamweb.nl>
2016-07-30 12:29:08 +0200

    yubi update

--- yubikey.zsh
+++ yubikey.zsh
@@ -1,15 +1,15 @@
 # make it less tempting to use gpg version 1
 alias gpg=gpg2
 # use gpg's tab completions for gpg2; careful: not all options exist in both
-compdef gpg2=gpg
+#compdef gpg2=gpg
 
 # Yubikey , GPG, SSH
 if [ -x /usr/bin/gpg-agent ]; then
-  echo "gpg-agent exists"
+#  echo "gpg-agent exists"
   if [[ ! $(ps x | grep gpg-agent | grep ssh | grep -v grep) ]]; then
-    echo "no gpg-agent exists"
+#    echo "no gpg-agent exists"
     while [[ $(ps x | grep gpg-agent | grep -v grep) ]]; do
-      echo "trying to kill the agent"
+#      echo "trying to kill the agent"
       killall gpg-agent > /dev/null 2>&1
     done
     #if [ ! -f $HOME/.gpg-agent-info ]; then
