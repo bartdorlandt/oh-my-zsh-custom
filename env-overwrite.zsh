53fa215e9b5a3eb524459cd816f96717fb294436
root@bartlegian <bart@bamweb.nl>
2016-08-15 13:54:34 +0200

    env

--- env-overwrite.zsh
+++ env-overwrite.zsh
@@ -1,7 +1,8 @@
 ZSH_THEME="tjkirch"
 CASE_SENSITIVE="true"
 # Do not prompt me about auto updates, just do it.
-DISABLE_UPDATE_PROMPT=true
+DISABLE_UPDATE_PROMPT="true"
+DISABLE_AUTO_UPDATE="false"
 
 # Load the bash aliases I already had.
 source $HOME/.bash_aliases
