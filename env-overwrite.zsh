2086647620711d9f447e0f31ea245ba8757dc9c3
bart@bambam <bart@bamweb.nl>
2016-09-04 11:55:44 +0200

    env update

--- env-overwrite.zsh
+++ env-overwrite.zsh
@@ -8,7 +8,7 @@ CASE_SENSITIVE="true"
 plugins=(git debian)
 # Do not prompt me about auto updates, just do it.
 DISABLE_UPDATE_PROMPT="true"
-DISABLE_AUTO_UPDATE="false"
+DISABLE_AUTO_UPDATE="true"
 
 # Load the bash aliases I already had.
 source $HOME/.bash_aliases
