ae59a688a994408a7ba62db4802ce6e0c37f578b
root@hermione-3 <bart@bamweb.nl>
2018-05-20 17:06:24 +0200

    path root update

--- path_root.zsh
+++ path_root.zsh
@@ -5,10 +5,10 @@ if [ $UID -eq 0 ]; then
 
 	# set PATH so it includes user's private bin if it exists
 	if [ -d $HOME/bin ] ; then 
-		PATH="${PATH}:/$HOME/bin"
+		PATH="${PATH}:$HOME/bin"
 	fi                                                               
 	# set PATH so it includes user's private scripts if it exists
 	if [ -d $HOME/scripts ] ; then
-		PATH="${PATH}:/$HOME/scripts"
+		PATH="${PATH}:$HOME/scripts"
 	fi
 fi
