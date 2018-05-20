9479bf9b02291b736edbe2d3a77468b0e1b79b0d
root@harry2.bamweb.nl <bart@bamweb.nl>
2018-05-20 17:02:15 +0200

    path for root added

---
+++ path_root.zsh
@@ -0,0 +1,14 @@
+# setting the path for root users
+
+if [ $UID -eq 0 ]; then
+	PATH="${PATH}:/sbin:/bin:/usr/sbin:/usr/bin"
+
+	# set PATH so it includes user's private bin if it exists
+	if [ -d $HOME/bin ] ; then 
+		PATH="${PATH}:/$HOME/bin"
+	fi                                                               
+	# set PATH so it includes user's private scripts if it exists
+	if [ -d $HOME/scripts ] ; then
+		PATH="${PATH}:/$HOME/scripts"
+	fi
+fi
