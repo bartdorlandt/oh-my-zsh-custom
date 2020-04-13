0a5205dd62b8eb70bf528f5ae26ff76f785ac941
bart@bambam <bart@bamweb.nl>
2020-04-13 23:02:10 +0200

    added platform tools path, if exists

--- env-overwrite.zsh
+++ env-overwrite.zsh
@@ -21,6 +21,9 @@ fi
 if [ -d $HOME/bin ] ; then
     PATH="$HOME/bin:$PATH"
 fi
+if [ -d $HOME/bin/platform-tools ] ; then
+    PATH="$PATH:$HOME/bin/platform-tools"
+fi
 if [ -d $HOME/.local/bin ] ; then
     PATH="$HOME/.local/bin:$PATH"
 fi
